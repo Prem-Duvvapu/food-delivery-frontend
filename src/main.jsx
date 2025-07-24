fd2aeaa78b7952c2de8ee01a8fcc3bae13793b9c
Prem-Duvvapu <prem.duvvapu@gmail.com>
2025-07-24 19:23:29 +0000

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
+import Home from './pages/Home';
+// import Restaurant from './pages/Restaurant';
+// import Cart from './pages/Cart';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+        {/* <Route path="restaurant/:id" element={<Restaurant />} />
+        <Route path="cart" element={<Cart />} /> */}
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="p-4">
+      <h1 className="text-3xl font-bold">Welcome to Food Delivery App</h1>
+      {/* Later: list of restaurants */}
+    </div>
+  );
+}
